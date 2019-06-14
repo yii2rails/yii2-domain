e392bd4b44f214332840c49fcbe74a2e2cb10398
Vitaliy <theyamshikov@yandex.ru>
2019-06-14 10:38:25 +0600

    fix: revert boolean type

--- src/helpers/types/BooleanType.php
+++ src/helpers/types/BooleanType.php
@@ -5,12 +5,12 @@ namespace yii2rails\domain\helpers\types;
 class BooleanType extends BaseType {
 	
 	protected function _isValid($value, $params = null) {
-        $value = $this->prepare($value);
+        //$value = $this->prepare($value);
 		return is_numeric($value) || is_bool($value);
 	}
 	
 	public function normalizeValue($value, $params = null) {
-	    $value = $this->prepare($value);
+	    //$value = $this->prepare($value);
 		$value = boolval($value);
 		return $value;
 	}
